cdd9706b9677a3f77f0848941781b2a6f9d9c553
abcerrar <davidcoleraplana@gmail.com>
2023-10-12 03:32:07 +0200

    normal paco done

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: dcolera- <dcolera-@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 19:31:54 by dcolera-          #+#    #+#             */
-/*   Updated: 2023/10/12 03:17:57 by dcolera-         ###   ########.fr       */
+/*   Updated: 2023/10/12 03:31:51 by dcolera-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,20 @@ char	*ft_strjoin(char *s1, char *s2)
 	size_t	total_length;
 	char	*str;
 	size_t	i;
+	size_t	len_s1;
 
 	i = -1;
-	total_length = ft_strlen(s1) + ft_strlen(s2) + 1;
+	len_s1 = ft_strlen(s1);
+	total_length = len_s1 + ft_strlen(s2) + 1;
 	str = (char *)malloc(total_length);
 	if (!str)
 		return (free(s1), NULL);
 	while (++i < total_length)
 	{
-		if (i < ft_strlen(s1))
+		if (i < len_s1)
 			str[i] = s1[i];
 		else
-			str[i] = s2[i - ft_strlen(s1)];
+			str[i] = s2[i - len_s1];
 	}
 	str[i - 1] = 0;
 	free(s1);
