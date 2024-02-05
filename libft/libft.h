8a454b085299220e0ae1ac0f9d77c240859687da
David <davidcoleraplana2@gmail.com>
2024-02-05 15:58:18 +0100

    printf added to libft

--- libft/libft.h
+++ libft/libft.h
@@ -17,6 +17,7 @@
 # include <string.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdarg.h>
 
 typedef struct s_list
 {
@@ -68,4 +69,6 @@ void		ft_lstdelone(t_list *lst, void (*del)(void *));
 void		ft_lstclear(t_list **lst, void (*del)(void *));
 void		ft_lstiter(t_list *lst, void (*f)(void *));
 t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+int	ft_printf(char const *str, ...);
 #endif
